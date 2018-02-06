645926e8afe414ade437c64cec1615a5c4b234d0
codincodee <codincodee@outlook.com>
2018-02-06 01:05:21 -0800

    Initial

---
+++ main_window.cpp
@@ -0,0 +1,34 @@
+#include "main_window.h"
+#include "ui_main_window.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+  QMainWindow(parent),
+  ui(new Ui::MainWindow)
+{
+  ui->setupUi(this);
+  QFont font = ui->WordLabel->font();
+  font.setBold(true);
+  font.setPointSize(30);
+  ui->WordLabel->setFont(font);
+
+  font = ui->IKnowTheWordPushButton->font();
+  font.setBold(true);
+  font.setPointSize(10);
+  ui->IKnowTheWordPushButton->setFont(font);
+
+  font = ui->IDontKnowTheWordPushButton->font();
+  font.setBold(true);
+  font.setPointSize(10);
+  ui->IDontKnowTheWordPushButton->setFont(font);
+
+  ui->MeaningLabel->setText("");
+  font = ui->MeaningLabel->font();
+  font.setBold(true);
+  font.setPointSize(20);
+  ui->MeaningLabel->setFont(font);
+}
+
+MainWindow::~MainWindow()
+{
+  delete ui;
+}
